d1b4b3310305b2efc31b0d470e2ea0adc1274d95
kokanesuraj007 <surajkokane1997@gmail.com>
2022-08-25 22:36:55 +0530

    updatefile

---
+++ Scripts/dist/navbar.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _navbar = _interopRequireDefault(require("../components/navbar.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+// Navbar script
+document.querySelector("#navbar").innerHTML = (0, _navbar["default"])();
+//# sourceMappingURL=navbar.dev.js.map
